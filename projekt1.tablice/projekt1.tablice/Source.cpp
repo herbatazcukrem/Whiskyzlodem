a395a5d934391554e39c0df466f4d9eeb5dea424
Karolina <Karolina@THINKPAD>
2016-11-19 10:51:29 +0100

    p.tablice3

--- projekt1.tablice/projekt1.tablice/Source.cpp
+++ projekt1.tablice/projekt1.tablice/Source.cpp
@@ -3,9 +3,10 @@
 
 using namespace std;
 
-const int roz = 5;
-int t1[roz], t2[roz], t3[roz];
+const int roz = 1000;
 
+const int roz2 = 2000;
+int t1[roz], t2[roz], t3[roz], t4[2 * roz] = { 0 };
 
 void Wypisz(int t[], int roz)						// dopisaæ niewypisywanie nieznacz¹cych zer z przodu
 {
@@ -32,6 +33,7 @@ void Stworz(int t[], int roz)						//OK
 
 void Dodawanie(int t1[], int t2[], int t3[], int roz)	//OK
 {
+
 	int q=0;
 	for (int i = roz - 1; i >= 0; i--)
 	{
@@ -52,6 +54,7 @@ void Dodawanie(int t1[], int t2[], int t3[], int roz)	//OK
 void Odejmowanie(int t1[], int t2[], int t3[], int roz)			//
 {
 	
+	
 	for (int i = roz - 1; i >= 0; i--)
 	{
 		if ((t2[i] > t1[i]) && (i!=0))
@@ -66,11 +69,40 @@ void Odejmowanie(int t1[], int t2[], int t3[], int roz)			//
 }
 
 
+void Mnozenie(int t1[], int t2[], int t3[])
+{
+	
+	int t[roz2] = { 0 };
+	cout << "roz " << roz << "    roz2 " << roz2<<endl;
+	int k = roz2-1; int u = roz2-1; int l = 0; int p = 0;
+	for (int i = roz - 1; i >= 0; i--)
+	{
+		
+		for (int j = roz - 1; j >= 0; j--)
+		{
 
+			//k =cout << k << "   ";
+			t[u] = t2[i] * t1[j]; // k; cout << t[u] << endl;
+			u--;
+		}
+	
+	//	Wypisz(t, roz2);
+		Dodawanie(t, t3, t3, roz2);
+		t[k]=0;
+		k--;
+		u = k;
+		
+	}
+
+	cout << "ble" << endl;
+
+
+}
 
 
 int main()
 {
+//	const int *rozmiar = &roz;
 	srand(time(NULL));
 	cout << "tablica 1: ";
 	Stworz(t1, roz);
@@ -79,9 +111,10 @@ int main()
 	//Dodawanie(t1, t2,t3, roz);
 	//cout << "tablica 3: ";
 	//Wypisz(t3, roz);
-	Odejmowanie(t1, t2, t3, roz);
+	//Odejmowanie(t1, t2, t3, roz);
+	Mnozenie(t1, t2, t4);
 	cout << "tablica 3: ";
-	Wypisz(t3, roz);
+	Wypisz(t4, 2*roz);
 	cin.get();
 	return 1;
 }
