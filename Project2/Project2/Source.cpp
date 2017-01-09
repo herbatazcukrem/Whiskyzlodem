09343fc121ece621fdfd6f40f26255ae8f44a7f1
Karolina <Karolina@THINKPAD>
2017-01-09 16:21:16 +0100

    b

--- Project2/Project2/Source.cpp
+++ Project2/Project2/Source.cpp
@@ -15,45 +15,107 @@ struct sdzien {
 };
 
 
-int main()
+bool porwnajd(sdzien k1, sdzien k2)
 {
-	sdzien k1;
-	sdzien k2;
-	cin >> k1.dz >> k1.mies >> k1.rok;
-	cin >> k2.dz >> k2.mies >> k2.rok;
-	cout << k1.dz << "." << k1.mies << "." << k1.rok<<endl;
-	cout << k2.dz << "." << k2.mies << "." << k2.rok<<endl;
-	
 	if (k1.rok == k2.rok)
-	
-		
-		if (k1.mies = k2.mies)
+
+
+		if (k1.mies == k2.mies)
 		{
 			if (k1.dz > k2.dz)
 			{
-				
-				cout << k1.dz << "." << k1.mies << "." << k1.rok << "k1.dz = k2.dz";
+
+				cout << k1.dz << "." << k1.mies << "." << k1.rok << "k1.dz = k2.dz"; return true;
 			}
 			else
 			{
 				if (k1.dz > k2.dz)
-					cout << k1.dz << "." << k1.mies << "." << k1.rok<<"k1.dz > k2.dz)";
-				else cout << k2.dz << "." << k2.mies << "." << k2.rok<<"else k1.dz > k2.dz";
+				{
+					cout << k1.dz << "." << k1.mies << "." << k1.rok << "k1.dz > k2.dz)"; return true;
+				}
+				else { cout << k2.dz << "." << k2.mies << "." << k2.rok << "else k1.dz > k2.dz"; return false; }
 
 			}
 		}
 		else
+		{
+			if (k1.mies > k2.mies)
 			{
-				if (k1.mies > k2.mies)
-					cout << k1.dz << "." << k1.mies << "." << k1.rok<<"k1.mies > k2.mies";
-				else cout << k2.dz << "." << k2.mies << "." << k2.rok<<" else k1.mies > k2.mies" ;
+				cout << k1.dz << "." << k1.mies << "." << k1.rok << "k1.mies > k2.mies"; return true;
 			}
-		
-	
+			else { cout << k2.dz << "." << k2.mies << "." << k2.rok << " else k1.mies > k2.mies"; return false; }
+		}
+
+
 	else if (k1.rok > k2.rok)
-		cout << k1.dz << "." << k1.mies << "." << k1.rok<<"k1.rok > k2.rok";
-		else  cout << k2.dz << "." << k2.mies << "." << k2.rok << "else k1.rok > k2.rok";
+	{
+		cout << k1.dz << "." << k1.mies << "." << k1.rok << "k1.rok > k2.rok"; return true;
+	}
+	else { cout << k2.dz << "." << k2.mies << "." << k2.rok << "else k1.rok > k2.rok"; return false; }
+
+
+}
+
+
+bool porwnajg(sgodz k1, sgodz k2)
+{
 	
+		if (k1.godz == k2.godz)
+		{
+			if (k1.min > k2.min)
+			{
+
+				cout << k1.godz << ":" << k1.min ; return true;
+			}
+			else
+			{
+				if (k1.min > k2.min)
+				{
+					cout << k1.godz << ":" << k1.min; return true;
+				}
+				else { cout << k2.godz << ":" << k2.min ; return false; }
+
+			}
+		}
+		else
+		{
+			if (k1.godz > k2.godz)
+			{
+				cout << k1.godz<< ":" << k1.min; return true;
+			}
+			else { cout << k2.godz << ":" << k2.min; return false; }
+		}
+
+
+	
+	
+
+
+}
+
+int main()
+{
+	sdzien k1;
+	sdzien k2;
+	sgodz p1;
+	sgodz p2;
+//	cin >> k1.dz>> k1.mies >> k1.rok;
+	//cin >> k2.dz >> k2.mies >> k2.rok;
+	//cout << k1.dz << "." << k1.mies << "." << k1.rok<<endl;
+	//cout << k2.dz << "." << k2.mies << "." << k2.rok<<endl;
+	
+	cin >> p1.godz >> p1.min;
+	cin >> p2.godz >> p2.min;
+	if (porwnajg(p1, p2))
+		cout << "wieksza k1";
+	else cout << "mniejsza k1";
+
+	
+//	if (porwnajd(k1, k2))
+//		cout << "wieksza k1";
+//	else cout << "mniejsza k1";
+
+
 	cin.get(); cin.get();
 	return 0;
 }
